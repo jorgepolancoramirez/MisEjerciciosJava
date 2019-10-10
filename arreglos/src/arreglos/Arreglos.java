5aefcc39dab60c7dc4fa90c7ef92588200add921
jorgepolancoramirez <jorgepolancoramirez@gmail.com>
2019-10-10 16:42:52 -0500

    agrefando modificaciones arreglo

--- arreglos/src/arreglos/Arreglos.java
+++ arreglos/src/arreglos/Arreglos.java
@@ -5,16 +5,20 @@ public class Arreglos {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double totalnotas=0;
         int [] notas = new int [5];
         for (int estudiantes = 0; estudiantes<=4;estudiantes++){
             System.out.println("ingrese la calificacion del estudiante n°" + (estudiantes + 1) + ": ");
                     notas [estudiantes] = input.nextInt();
+                    totalnotas += notas [estudiantes];
         }
-        
+        totalnotas = totalnotas / 5;
         for (int estudiantes = 0; estudiantes<=4; estudiantes++ ) {
             System.out.println("estudiante n°" + (estudiantes + 1) + ": " + notas [estudiantes]);
            
         }
+     System.out.println("el promedio de las notas es:" + totalnotas);
+
     }
     
 }
